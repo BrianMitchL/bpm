a7a754700135edbf83ab9e68a02131962f8f6dfe
Brian Mitchell <bman4789@gmail.com>
2016-04-09 15:04:31 -0500

    Layout and styling!

--- BPM/BPM/BMButton.swift
+++ BPM/BPM/BMButton.swift
@@ -2,17 +2,226 @@
 //  BMButton.swift
 //  BPM
 //
-//  Created by Brian Mitchell on 4/9/16.
-//  Copyright © 2016 Brian Mitchell. All rights reserved.
+//  Created by Amornchai Kanokpullwad on 4/9/2016.
+//  Copyright (c) 2016 Brian Mitchell and Zach Litzinger. All rights reserved.
+// 
+//  This was originally released as ZFRippleButton and can be found at
+//  https://github.com/zoonooz/ZFRippleButton
+//  The MIT License (MIT)
+//
+//  Copyright (c) 2014 Amornchai Kanokpullwad
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
 //
 
-import Foundation
 import UIKit
+import QuartzCore
 
-class BMButton: UIButton {
-    override func drawRect(rect: CGRect) {
-        var path = UIBezierPath(ovalInRect: rect)
-        UIColor.greenColor().setFill()
+@IBDesignable
+public class BMButton: UIButton {
+    
+    override public func drawRect(rect: CGRect) {
+        let path = UIBezierPath(ovalInRect: rect)
+        UIColor(hue:0, saturation:0, brightness:0.5, alpha:0.5).setFill()
         path.fill()
     }
+    
+    @IBInspectable public var ripplePercent: Float = 0.8 {
+        didSet {
+            setupRippleView()
+        }
+    }
+    
+    @IBInspectable public var rippleColor: UIColor = UIColor(white: 0.9, alpha: 1) {
+        didSet {
+            rippleView.backgroundColor = rippleColor
+        }
+    }
+    
+    @IBInspectable public var rippleBackgroundColor: UIColor = UIColor(white: 0.95, alpha: 1) {
+        didSet {
+            rippleBackgroundView.backgroundColor = rippleBackgroundColor
+        }
+    }
+    
+    @IBInspectable public var buttonCornerRadius: Float = 0 {
+        didSet{
+            layer.cornerRadius = CGFloat(buttonCornerRadius)
+        }
+    }
+    
+    @IBInspectable public var rippleOverBounds: Bool = false
+    @IBInspectable public var shadowRippleRadius: Float = 1
+    @IBInspectable public var shadowRippleEnable: Bool = true
+    @IBInspectable public var trackTouchLocation: Bool = false
+    @IBInspectable public var touchUpAnimationTime: Double = 0.6
+    
+    let rippleView = UIView()
+    let rippleBackgroundView = UIView()
+    
+    private var tempShadowRadius: CGFloat = 0
+    private var tempShadowOpacity: Float = 0
+    private var touchCenterLocation: CGPoint?
+    
+    private var rippleMask: CAShapeLayer? {
+        get {
+            if !rippleOverBounds {
+                let maskLayer = CAShapeLayer()
+                maskLayer.path = UIBezierPath(roundedRect: bounds,
+                                              cornerRadius: layer.cornerRadius).CGPath
+                return maskLayer
+            } else {
+                return nil
+            }
+        }
+    }
+    
+    convenience init() {
+        self.init(frame: CGRect.zero)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    private func setup() {
+        setupRippleView()
+        
+        rippleBackgroundView.backgroundColor = rippleBackgroundColor
+        rippleBackgroundView.frame = bounds
+        layer.addSublayer(rippleBackgroundView.layer)
+        rippleBackgroundView.layer.addSublayer(rippleView.layer)
+        rippleBackgroundView.alpha = 0
+        
+        layer.shadowRadius = 0
+        layer.shadowOffset = CGSize(width: 0, height: 1)
+        layer.shadowColor = UIColor(white: 0.0, alpha: 0.5).CGColor
+    }
+    
+    private func setupRippleView() {
+        let size: CGFloat = CGRectGetWidth(bounds) * CGFloat(ripplePercent)
+        let x: CGFloat = (CGRectGetWidth(bounds)/2) - (size/2)
+        let y: CGFloat = (CGRectGetHeight(bounds)/2) - (size/2)
+        let corner: CGFloat = size/2
+        
+        rippleView.backgroundColor = rippleColor
+        rippleView.frame = CGRectMake(x, y, size, size)
+        rippleView.layer.cornerRadius = corner
+    }
+    
+    override public func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
+        if trackTouchLocation {
+            touchCenterLocation = touch.locationInView(self)
+        } else {
+            touchCenterLocation = nil
+        }
+        
+        UIView.animateWithDuration(0.1, delay: 0, options: UIViewAnimationOptions.AllowUserInteraction, animations: {
+            self.rippleBackgroundView.alpha = 1
+            }, completion: nil)
+        
+        rippleView.transform = CGAffineTransformMakeScale(0.5, 0.5)
+        
+        
+        UIView.animateWithDuration(0.7, delay: 0, options: [UIViewAnimationOptions.CurveEaseOut, UIViewAnimationOptions.AllowUserInteraction],
+                                   animations: {
+                                    self.rippleView.transform = CGAffineTransformIdentity
+            }, completion: nil)
+        
+        if shadowRippleEnable {
+            tempShadowRadius = layer.shadowRadius
+            tempShadowOpacity = layer.shadowOpacity
+            
+            let shadowAnim = CABasicAnimation(keyPath:"shadowRadius")
+            shadowAnim.toValue = shadowRippleRadius
+            
+            let opacityAnim = CABasicAnimation(keyPath:"shadowOpacity")
+            opacityAnim.toValue = 1
+            
+            let groupAnim = CAAnimationGroup()
+            groupAnim.duration = 0.7
+            groupAnim.fillMode = kCAFillModeForwards
+            groupAnim.removedOnCompletion = false
+            groupAnim.animations = [shadowAnim, opacityAnim]
+            
+            layer.addAnimation(groupAnim, forKey:"shadow")
+        }
+        return super.beginTrackingWithTouch(touch, withEvent: event)
+    }
+    
+    override public func cancelTrackingWithEvent(event: UIEvent?) {
+        super.cancelTrackingWithEvent(event)
+        animateToNormal()
+    }
+    
+    override public func endTrackingWithTouch(touch: UITouch?, withEvent event: UIEvent?) {
+        super.endTrackingWithTouch(touch, withEvent: event)
+        animateToNormal()
+    }
+    
+    private func animateToNormal() {
+        UIView.animateWithDuration(0.1, delay: 0, options: UIViewAnimationOptions.AllowUserInteraction, animations: {
+            self.rippleBackgroundView.alpha = 1
+            }, completion: {(success: Bool) -> () in
+                UIView.animateWithDuration(self.touchUpAnimationTime, delay: 0, options: UIViewAnimationOptions.AllowUserInteraction, animations: {
+                    self.rippleBackgroundView.alpha = 0
+                    }, completion: nil)
+        })
+        
+        
+        UIView.animateWithDuration(0.7, delay: 0,
+                                   options: [.CurveEaseOut, .BeginFromCurrentState, .AllowUserInteraction],
+                                   animations: {
+                                    self.rippleView.transform = CGAffineTransformIdentity
+                                    
+                                    let shadowAnim = CABasicAnimation(keyPath:"shadowRadius")
+                                    shadowAnim.toValue = self.tempShadowRadius
+                                    
+                                    let opacityAnim = CABasicAnimation(keyPath:"shadowOpacity")
+                                    opacityAnim.toValue = self.tempShadowOpacity
+                                    
+                                    let groupAnim = CAAnimationGroup()
+                                    groupAnim.duration = 0.7
+                                    groupAnim.fillMode = kCAFillModeForwards
+                                    groupAnim.removedOnCompletion = false
+                                    groupAnim.animations = [shadowAnim, opacityAnim]
+                                    
+                                    self.layer.addAnimation(groupAnim, forKey:"shadowBack")
+            }, completion: nil)
+    }
+    
+    override public func layoutSubviews() {
+        super.layoutSubviews()
+        
+        setupRippleView()
+        if let knownTouchCenterLocation = touchCenterLocation {
+            rippleView.center = knownTouchCenterLocation
+        }
+        
+        rippleBackgroundView.layer.frame = bounds
+        rippleBackgroundView.layer.mask = rippleMask
+    }
+    
 }
