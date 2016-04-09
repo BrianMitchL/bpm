441c313f38fd046cf727985823466e806f0ab19b
Zach Litzinger <z_wolfe@mac.com>
2016-04-09 12:12:07 -0500

    Made custom button a circle

--- BPM/BPM/BMButton.swift
+++ BPM/BPM/BMButton.swift
@@ -10,13 +10,9 @@ import Foundation
 import UIKit
 
 class BMButton: UIButton {
-    required init(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)!
-//        self.layer.cornerRadius = 0.5 * self.layer.bounds.size.width
-//        self.layer.borderColor = UIColor.redColor().CGColor
-//        self.layer.borderWidth = 1.5
-//        self.backgroundColor = UIColor.blueColor()
-//        self.tintColor = UIColor.whiteColor()
-        
+    override func drawRect(rect: CGRect) {
+        var path = UIBezierPath(ovalInRect: rect)
+        UIColor.greenColor().setFill()
+        path.fill()
     }
 }
---
+++ BPM/BPM/PushButtonView.swift
@@ -0,0 +1,21 @@
+//
+//  PushButtonView.swift
+//  BPM
+//
+//  Created by ZachHacks on 4/9/16.
+//  Copyright © 2016 Brian Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class PushButtonView: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
