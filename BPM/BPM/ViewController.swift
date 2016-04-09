664c0c15cbf3ca257fb0be4ec4cf41bfa9e2fb23
Brian Mitchell <bman4789@gmail.com>
2016-04-08 21:45:51 -0500

    Initial commit

---
+++ BPM/BPM/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  BPM
+//
+//  Created by Brian Mitchell and Zach Litzinger on 4/8/16.
+//  Copyright © 2016 Brian Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func bpmTap(sender: UIButton) {
+        
+    }
+
+    @IBOutlet weak var bpmDisplay: UITextField!
+    
+    
+}
+
