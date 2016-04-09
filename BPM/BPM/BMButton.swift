fcb30bbbc7f5e9a196c21fa5dcc74e4e338e969c
Zach Litzinger <z_wolfe@mac.com>
2016-04-09 10:30:57 -0500

    Adding button with scaling

--- BPM/BPM/BMButton.swift
+++ BPM/BPM/BMButton.swift
@@ -12,7 +12,7 @@ import UIKit
 class BMButton: UIButton {
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
-        self.layer.cornerRadius = 0.5 * self.layer.bounds.size.width
+//        self.layer.cornerRadius = 0.5 * self.layer.bounds.size.width
 //        self.layer.borderColor = UIColor.redColor().CGColor
 //        self.layer.borderWidth = 1.5
 //        self.backgroundColor = UIColor.blueColor()
