3b4d2548b8570b5bf70d39b33931a0e5e251a63f
Zach Litzinger <z_wolfe@mac.com>
2016-04-09 20:00:36 -0500

    Implementing Kalman Filter

--- BPM/BPM/ViewController.swift
+++ BPM/BPM/ViewController.swift
@@ -23,29 +23,45 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    var average: Double = 0
-    var count = 0
+    var X: Double = 1         //Initial Duration between the taps
+    var P: Double = 0.1       //Initial confidence in the measurements of the values of x
+    var Q: Double = 0.00001   //Constant error: you know that you are often slow
+    var A: Double = 1         // ------- might be important?
+    var H: Double = 0.001      //How accurately we believe the instrument is measuring
+    var R: Double = 0.00001 //Speed of response to variants
+    
     var lastTime: NSDate? = nil
     
 
     @IBAction func bpmTap(sender: BMButton) {
         let currentTime = NSDate()
-        if count < 1 {
+        if lastTime == nil {
             lastTime = NSDate(timeIntervalSinceNow: -1)
         }
         let timeDiff = currentTime.timeIntervalSinceDate(lastTime!)
-        let newAverage = calculateBPM(timeDiff, average: average, newCount: count)
-        count += 1
-        average = newAverage
+        let calculation = correct(timeDiff)
         lastTime = currentTime
-        bpmDisplay.text = "\(Int(average))"
+        bpmDisplay.text = "\(Int(60.0 / calculation))"
     }
     
-    func calculateBPM(timeDiff: NSTimeInterval, average: Double, newCount: Int) -> Double {
-        if newCount > 2 {
-            count = 1
-        }
-        return ((60/timeDiff) + Double(average) * Double(newCount)) / Double(newCount + 1)
+    func predict() {
+        X = A * X
+        P = A * P + Q
     }
+    
+    func correct(measurement: Double) -> Double {
+        predict()
+        let K = P * (1 / (P + R))
+        X = X + K * (measurement - X)
+        P = (1 - K) * P
+        return X
+    }
+    
+//    func calculateBPM(timeDiff: NSTimeInterval, average: Double, newCount: Int) -> Double {
+//        if newCount > 2 {
+//            count = 1
+//        }
+//        return ((60/timeDiff) + Double(average) * Double(newCount)) / Double(newCount + 1)
+//    }
 }
 
